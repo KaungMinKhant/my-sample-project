d0b84fd1110dd44013fee12a642d591d0bb8c77b
KaungMinKhant <phenomenalkaung@gmail.com>
2018-10-16 00:38:55 +0630

    Widget Model, Widget Create

--- routes/web.php
+++ routes/web.php
@@ -10,16 +10,25 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+// Authenication Route
 Auth::routes(['verify' => true]);
-// home page route
+
+// Home Page Route
 Route::get('/', 'PagesController@index')->middleware('verified');
 
-// Test Route
-Route::get('test', 'TestController@index')->middleware('verified');
+// Logout Route
 Route::get('logout', function(){
 	auth()->logout();
 	return view('auth.login');
 });
+
+// Test Route
+Route::get('test', 'TestController@index')->middleware('verified');
+
+// Widget Route
+Route::resource('widget', 'WidgetController');
+
+
 
 
 
